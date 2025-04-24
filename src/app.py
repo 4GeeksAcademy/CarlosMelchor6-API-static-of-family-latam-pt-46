95902f5d1cb126ab939bffb7bc24ce3bd54dba32
Carlos Melchor Perez <carloos0665@gmail.com>
2025-04-24 00:05:23 +0000

    GET 3 leng of family

--- src/app.py
+++ src/app.py
@@ -16,6 +16,31 @@ CORS(app)
 # Create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+John = {
+    "first_name": "John",
+    "age": 33,
+    "last_name": jackson_family.last_name,
+    "lucky_numbers": [7, 13, 22]
+}
+
+Jane = {
+    "first_name": "Jane",
+    "last_name": jackson_family.last_name,
+    "age": 35,
+    "lucky_numbers": [10, 14, 3]
+}
+
+Jimmy = {
+    "first_name": "Jimmy",
+    "last_name": jackson_family.last_name,
+    "age": 5,
+    "lucky_numbers": [1]
+}
+
+jackson_family.add_member(John)
+jackson_family.add_member(Jane)
+jackson_family.add_member(Jimmy)
+
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -31,12 +56,8 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-    # This is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
-
+    return jsonify(members), 200
 
 
 # This only runs if `$ python src/app.py` is executed
